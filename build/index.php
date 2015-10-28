725452a1720f37e0df1d809c6aad080377ddb764
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2015-10-28 08:03:56 +0000

    Continue working on indexer. Why is it converting to html entities....?

--- build/index.php
+++ build/index.php
@@ -1268,46 +1268,26 @@ register_module([
 			
 			$index = [];
 			
+			// Regex from 
+			$terms = preg_split("/((^\p{P}+)|(\p{P}*\s+\p{P}*)|(\p{P}+$))/", $source, -1, PREG_SPLIT_NO_EMPTY);
+			$i = 0;
+			foreach($terms as $term)
+			{
+				$nterm = strtolower($term);
+				if(!isset($index[$nterm]))
+				{
+					$index[$nterm] = [ "freq" => 0, "offsets" => [] ];
+				}
+				
+				$index[$nterm]["freq"]++;
+				$index[$nterm]["offsets"][] = $i;
+				
+				$i++;
+			}
+			
 			var_dump($env->page);
 			var_dump($source);
 			echo("source length: $source_length\n");
-			
-			$basepos = 0;
-			$scanpos = $basepos;
-			while($basepos < $source_length)
-			{
-				$word = "";
-				do {
-					// Break if we reach the end of the source text
-					if($scanpos >= $source_length) break;
-					$word .= $source[$scanpos];
-					$scanpos++;
-				} while(strpos($breakable_chars, $source[$scanpos]) === false);
-				
-				// Move the base position up to the scan position (plus one to
-				// skip over the breakable character), saving the old base
-				// position for later
-				$word_start_pos = $basepos;
-				$basepos = $scanpos + 1;
-				// Continue if the word is empty
-				if(strlen($word) === 0) continue;
-				// Normalise the word to be lowercase
-				$word = strtolower($word);
-				
-				var_dump($word);
-				
-				// Initialise the entry in the index if it doesn't exist
-				if(!isset($index[$word]))
-				{
-					$index[$word] = [
-						"freq" => 0,
-						"offsets" => []
-					];
-				}
-				// Update the index entry
-				$index[$word]["freq"]++;
-				$index[$word]["offsets"][] = $word_start_pos;
-			}
 			
 			var_dump($index);
 		});
