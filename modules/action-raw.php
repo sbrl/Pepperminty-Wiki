3c14063f0dc8c73045caaa1099f8440c606751b5
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2016-01-23 14:33:18 +0000

    Remove old 307 redirect from raw action.

--- modules/action-raw.php
+++ modules/action-raw.php
@@ -1,7 +1,7 @@
 <?php
 register_module([
 	"name" => "Raw page source",
-	"version" => "0.5",
+	"version" => "0.6",
 	"author" => "Starbeamrainbowlabs",
 	"description" => "Adds a 'raw' action that shows you the raw source of a page.",
 	"id" => "action-raw",
@@ -18,7 +18,6 @@ register_module([
 		add_action("raw", function() {
 			global $env;
 
-			http_response_code(307);
 			header("x-filename: " . rawurlencode($env->page) . ".md");
 			header("content-type: text/markdown");
 			exit(file_get_contents("$env->storage_prefix$env->page.md"));
