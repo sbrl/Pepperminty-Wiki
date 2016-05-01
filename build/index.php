a09d8c011542d2cb069a7c0a916422882e3813cf
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2016-05-01 15:17:49 +0100

    Widen more menu

--- build/index.php
+++ build/index.php
@@ -304,7 +304,7 @@ nav:not(.nav-more-menu) a { text-decoration: none; font-weight: bolder; color: i
 .nav-more label { cursor: pointer; }
 .nav-more-menu { display: none; position: absolute; flex-direction: column; top: 2.6rem; right: -0.2rem; background-color: #8a62a7; border-top: 3px solid #442772; border-bottom: 3px solid #442772;}
 input[type=checkbox]:checked ~ .nav-more-menu { display: block; box-shadow: 0.4rem 0.4rem 1rem 0 rgba(50, 50, 50, 0.5); }
-.nav-more-menu span { min-width: 8rem; }
+.nav-more-menu span { min-width: 10rem; }
 
 .inflexible { flex: none; }
 .off-screen { position: absolute; top: -1000px; left: -1000px;}
