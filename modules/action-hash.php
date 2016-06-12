97de1974aaad07a3619af16457cc6f0b6e1d6af9
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2016-06-12 13:40:28 +0100

    Update hash module.

--- modules/action-hash.php
+++ modules/action-hash.php
@@ -1,11 +1,18 @@
 <?php
 register_module([
 	"name" => "Password hashing action",
-	"version" => "0.5",
+	"version" => "0.6",
 	"author" => "Starbeamrainbowlabs",
 	"description" => "Adds a utility action (that anyone can use) called hash that hashes a given string. Useful when changing a user's password.",
 	"id" => "action-hash",
 	"code" => function() {
+		/**
+		 * @api {get} ?action=hash&string={text} Hash a password
+		 * @apiName Hash
+		 * @apiGroup Utility
+		 * @apiParam {string}	string	The string to hash.
+		 * @apiParam {boolean}	raw		Wherher to return the hashed password as a raw string instead of as part of an HTML page.
+		 */
 		
 		/*
 		 * ██   ██  █████  ███████ ██   ██ 
@@ -14,7 +21,6 @@ register_module([
 		 * ██   ██ ██   ██      ██ ██   ██ 
 		 * ██   ██ ██   ██ ███████ ██   ██
 		 */
-		
 		add_action("hash", function() {
 			global $settings;
 			
@@ -23,6 +29,11 @@ register_module([
 				http_response_code(422);
 				exit(page_renderer::render_main("Missing parameter", "<p>The <code>GET</code> parameter <code>string</code> must be specified.</p>
 		<p>It is strongly recommended that you utilise this page via a private or incognito window in order to prevent your password from appearing in your browser history.</p>"));
+			}
+			else if(!empty($_GET["raw"]))
+			{
+				header("content-type: text/plain");
+				exit(hash_password($_GET["string"]));
 			}
 			else
 			{
