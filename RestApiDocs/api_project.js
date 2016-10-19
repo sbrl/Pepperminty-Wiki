6d92c793aa81af021f1d6cbff74c35a804a13a93
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2016-10-19 20:59:37 +0100

    Apparently apidoc has a dumb version parser.

--- RestApiDocs/api_project.js
+++ RestApiDocs/api_project.js
@@ -1,13 +1,13 @@
 define({
   "name": "Pepperminty Wiki",
-  "version": "0.13-dev",
+  "version": "0.13",
   "description": "A wiki in a box. This is the API documentation.",
-  "title": "Pepperminty Wiki",
+  "title": "Pepperminty Wiki (0.13-dev)",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-10-19T19:50:59.501Z",
+    "time": "2016-10-19T19:59:10.420Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
