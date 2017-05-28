d02bccf14543d47a5c8556824a2088cc4523b409
Isabeau Kisler <ikisler@csumb.edu>
2017-05-28 15:45:05 -0700

    Fix repeated page names on sidebar - remove index.php
    Remove changes from build/index.php

--- build/index.php
+++ build/index.php
@@ -2068,9 +2068,6 @@ function render_sidebar($pageindex, $root_pagename = "")
 		if($pagename == $root_pagename)
 			continue;
 		
-		// If the page already appears on the sidebar, skip it
-		if(preg_match("/>$pagename<\a>/m",$result)===1)
-			continue;
 		
 		// If the part of the current pagename that comes after the root
 		// pagename has a slash in it, skip it as it is a sub-sub page.
