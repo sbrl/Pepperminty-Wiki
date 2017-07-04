c72d7dab829e2148661e77e3c1c3b4ffc1b24275
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-07-04 18:30:44 +0100

    Improve the look of the downloader a bit.

--- download.php
+++ download.php
@@ -5,11 +5,11 @@
 		<title>Pepperminty Wiki Download</title>
 	</head>
 	<body>
-		<h1>Pepperminty Wiki Download</h1>
+		<h1><img src="https://starbeamrainbowlabs.com/images/logos/peppermint.png" class="logo" /> Pepperminty Wiki Downloader</h1>
 		
 		<!-------------->
 		<h2>Module selector</h2>
-		<p>Choose the modules that you want to include in your installation of Pepperminty Wiki.</p>
+		<p>Choose the modules that you want to include in your installation of Pepperminty Wiki <?php echo(trim(file_get_contents("version"))); ?>.</p>
 		
 		<p>
 			<button onclick="select(true);">Select All</button>
@@ -51,18 +51,20 @@
 		<hr />
 		
 		<p>
-			Pepperminty Wiki was built by <a href="//starbeamrainbowlabs.com/">Starbeamrainbowlabs</a>. The code is available on <a href="//github.com/sbrl/pepperminty-wiki">GitHub</a>.
+			Pepperminty Wiki was built by <a href="https://starbeamrainbowlabs.com/"><img src="https://starbeamrainbowlabs.com/images/sbrl/SBRL-Small-64.png" class="logo" /> Starbeamrainbowlabs</a>. The code is available on <a href="//github.com/sbrl/pepperminty-wiki">GitHub</a>.
 		</p>
 		<p>
-			Other contributors: (none yet! Contribute and I will put your name here)
+			Other contributors: <a href="https://github.com/ikisler"><img src="https://avatars2.githubusercontent.com/u/12506147?v=3&s=32" class="logo" /> @ikisler</a>
 		</p>
 		
 		<!------------------->
 		<link rel="stylesheet" href="//starbeamrainbowlabs.com/theme/basic.css" />
 		<style>
 			body			{ padding: 1rem; color: #442772; background-colour: #eee8f2; } /* syntaxtic gets confused sometimes */
-			a 				{ color: #9e7eb4; }
+			a 				{ color: #9e7eb4;	}
 			.largebutton	{ font-size: 2rem;	}
+			
+			.logo			{ max-width: 1.25em; vertical-align: middle;	}
 		</style>
 		
 		<script>
