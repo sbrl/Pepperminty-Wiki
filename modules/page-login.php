50efd4bb497f3dda93705a63a93199e007b662e4
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2019-05-06 23:48:34 +0100

    Bump versions

--- modules/page-login.php
+++ modules/page-login.php
@@ -1,7 +1,7 @@
 <?php
 register_module([
 	"name" => "Login",
-	"version" => "0.9.2",
+	"version" => "0.9.3",
 	"author" => "Starbeamrainbowlabs",
 	"description" => "Adds a pair of actions (login and checklogin) that allow users to login. You need this one if you want your users to be able to login.",
 	"id" => "page-login",
