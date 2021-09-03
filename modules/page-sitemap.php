edd1702ea36dd95e2e2ac88a68c44b87d1784318
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-09-03 02:04:41 +0100

    page-sitemap: tweak description

--- modules/page-sitemap.php
+++ modules/page-sitemap.php
@@ -7,7 +7,7 @@ register_module([
 	"name" => "Sitemap",
 	"version" => "0.1",
 	"author" => "Starbeamrainbowlabs",
-	"description" => "Adds XML sitemap generation. Additional manual setup is required to notify search engines about the sitemap generated. See the Features FAQ in the documentation (or your wiki's credits page) for more information.",
+	"description" => "Adds XML sitemap generation. Additional manual setup is required to notify search engines about the sitemap. See the Features FAQ in the documentation (or your wiki's credits page) for more information.",
 	"id" => "page-sitemap",
 	"code" => function() {
 		global $settings;
